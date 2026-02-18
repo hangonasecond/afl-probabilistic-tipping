3bcb421926d64bbc27fb1584401f38178797795a
Nik Armstrong <nikalas.armstrong@outlook.com>
2026-02-18 17:33:39 +1100

    Create results_long and tidy Margin
    Create a new table, results_long, with Team and Opponent instead of
    Home.Team and Away.Team. Consequent variable renaming.
    
    Also apply a consequential fix for Margin so it reflects the team in
    Team instead of the home team.

---
+++ R/02-tidy-data.R
@@ -0,0 +1,78 @@
+library(arrow)
+library(tidyverse)
+
+fixtures <- open_dataset("data/raw/fixture", unify_schemas = TRUE) |> collect()
+ladders <- open_dataset("data/raw/ladder", unify_schemas = TRUE) |> collect()
+results <- open_dataset("data/raw/results", unify_schemas = TRUE) |> collect()
+
+# Exploration - results ---------------------------------------------------
+
+# First, get a feel for the data
+results_explore <- results |>
+  # include only regular season matches
+  filter(grepl("R", Round)) |>
+  group_by(Season) |>
+  mutate(Season.Prop = Round.Number / max(Round.Number)) |>
+  group_by(Home.Team) |>
+  mutate(Mean.Margin = mean(Margin)) |>
+  ungroup() |>
+  mutate(
+    Diff.Mean.Margin = Margin - Mean.Margin,
+    Winner = case_when(
+      Margin > 0 ~ Home.Team,
+      Margin < 0 ~ Away.Team,
+      Margin == 0 ~ "Draw"
+    )
+  )
+
+# Lengthen results so it is suitable for modelling
+# I want a df that allows me to filter for a team and see all their games,
+# both home and away, with a result.
+# We will create a long data set for home teams, and then for away teams,
+# And then join them.
+results_long_home <- results |>
+  mutate(
+    Team = Home.Team,
+    Opponent = Away.Team,
+    Home = TRUE,
+    .keep = "unused"
+  ) |>
+  rename(
+    Goals.For = Home.Goals,
+    Behinds.For = Home.Behinds,
+    Points.For = Home.Points,
+    Goals.Against = Away.Goals,
+    Behinds.Against = Away.Behinds,
+    Points.Against = Away.Points,
+  )
+
+results_long_away <- results |>
+  mutate(
+    Team = Away.Team,
+    Opponent = Home.Team,
+    Home = FALSE,
+    .keep = "unused"
+  ) |>
+  rename(
+    Goals.Against = Away.Goals,
+    Behinds.Against = Away.Behinds,
+    Points.Against = Away.Points,
+    Goals.For = Home.Goals,
+    Behinds.For = Home.Behinds,
+    Points.For = Home.Points,
+  )
+
+results_long <- bind_rows(results_long_home, results_long_away)
+
+# The margin should be the margin of the team in Team
+# We also want a result column for ease
+results_long <- results_long |>
+  mutate(
+    Margin = Home * Margin - (1 - Home) * Margin,
+    Result = case_when(
+      Margin > 0 ~ 1,
+      Margin == 0 ~ 0,
+      Margin < 0 ~ -1
+    ),
+    Result = factor(Result, labels = c("Win", "Draw", "Lose"))
+  )
